5a2b74812fb0beaafe9f04d689298432e52e3c1c
Jarrod Connolly <jarrod@nestedquotes.ca>
2018-07-31 21:24:45 -0700

    Initial commit. Add singly linked list implementation.

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+  },
+  "env": {
+    "es6": true,
+    "node": true,
+    "jest": true
+  },
+  "extends": "airbnb-base"
+};
